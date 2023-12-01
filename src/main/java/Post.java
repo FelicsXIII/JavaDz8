d9d341c8cb901342a57b1a82158f2d7de3e5b281
FelicsXIII <felics1689@gmail.com>
2023-12-01 15:43:04 +0300

    Correction Code

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,7 +1,6 @@
 public class Post {
     public FormDate birthday;
-
-    public String name;
+    public String name; // чтобы поле уже имело данные в этом классе что нужно прописать?
     public String passport;
     public String patronymic;
     public String phone;
